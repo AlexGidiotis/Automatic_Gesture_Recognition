09f2a0e6b15b6a12b049c913f3b9597dbe3e98fe
dimimal <dimimallios@gmail.com>
2016-10-18 16:44:23 +0300

    init file

---
+++ late_fusion_train.py
@@ -0,0 +1,8 @@
+#This scripts is used for late fusion scheme training
+
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
