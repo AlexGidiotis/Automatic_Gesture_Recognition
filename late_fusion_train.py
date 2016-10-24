1a6dc830299f989e4d0398350878abd3da5c3870
dimimal <dimimallios@gmail.com>
2016-10-24 23:54:34 +0300

    Init late fusion scheme

--- late_fusion_train.py
+++ late_fusion_train.py
@@ -1,8 +1,33 @@
-#This scripts is used for late fusion scheme training
+#This scripts Builds neural network for 
+#late fusion usage
+
 
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-# fix random seed for reproducibility
-seed = 7
-np.random.seed(seed)
+
+# load pima indians dataset
+dataset = numpy.loadtxt(, delimiter=",")
+
+#input = feature vector extracted 
+#keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+# create model
+model = Sequential()
+model.add(Dense(18, input_dim=6, init='uniform', activation='relu'))
+model.add(Dense(18, init='uniform', activation='relu'))
+model.add(Dense(12, init='uniform', activation='relu'))
+model.add(Dense(2, init='uniform', activation='relu'))
+
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, nb_epoch=150, batch_size=10,  verbose=2)
+
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# calculate predictions
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x) for x in predictions]
