e41f31e19bd71134ce655a7e708cb111fe2f2af3
AlexGidiotis <gidiotisAlex@outlook.com.gr>
2016-10-31 14:30:14 +0200

    create training script for htk training

---
+++ create_train_scp.py
@@ -0,0 +1,21 @@
+# Author: Alex Gidiotis
+#		  gidiotisAlex@outlook.com.gr
+
+# Creates the training script required for htk training.
+
+import os
+
+path_top = "C:\Users\Alex\Documents\University\Python\Data\MFC_data"
+path_out = "C:\Users\Alex\Documents\University\Python\Automatic_Gesture_Recognition\Training_Scripts"
+
+# Go to the data directory and write all .mfc files in the training script.
+of = open(path_out+"/Train.scp", 'w')
+# List the directory and open each file.
+listing = os.listdir(path_top)
+for f in listing:
+	# Ignore the master label file.
+	if f[-3:] == 'txt': continue
+	of.write("%s\\%s\n" %(path_top,f))
+
+print "Training script created."
+of.close()
