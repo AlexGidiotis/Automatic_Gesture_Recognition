95435b578b356f01978375321a4e61c37db8f652
AlexGidiotis <gidiotisAlex@outlook.com.gr>
2016-11-24 16:06:28 +0200

    alignment of skeletal and speech with activity detector

---
+++ Late_Fusion/align_skeletal_model.py
@@ -0,0 +1,70 @@
+import re
+import pandas as pd
+import numpy as np
+
+activity_file = 'C:\Users\Alex\Documents\University\Python\Automatic_Gesture_Recognition\\Test_labels.csv'
+
+print 'Loading activity labels...'
+af = pd.read_csv(activity_file)
+out_f = open('aligned_skeletal_recout.mlf','w')
+in_f = open('C:\Users\Alex\Documents\University\Python\Automatic_Gesture_Recognition\Training_Scripts\\recout.mlf', 'r')
+out_f.write("#!MLF!#\n")
+line = in_f.readline()
+
+# Read silences and put in dictionary
+sil_dict = {}
+list_file_nums = [int(fNum) for fNum in af['file'].unique()]
+for file in af['file'].unique():
+	df = af[af['file'] == file]
+	sil_labs = df[df['label'] == 'sil']
+	file_sil_list = []
+	file_index = str(int(file))
+	for k,g in sil_labs.groupby(sil_labs['frame'] - np.arange(sil_labs.shape[0])):
+		start,end = g.iloc[0]['frame'],g.iloc[-1]['frame']
+		file_sil_list.append((int(start * 100000),int(end * 100000),'SIL'))
+	sil_dict[file_index] = file_sil_list
+
+# Read prediction and put in dictionary
+input_dict = {}
+for line in in_f:
+	if line.startswith('\"\'*\''):
+		file_list = []
+		fileNum = re.findall('\"\'*\'\D*(\d+).rec',line)[0]
+		file_list.append(line)
+		
+	elif line.startswith('.'):
+		file_list.append(line)
+		input_dict[fileNum] = file_list
+	else:
+		word = re.findall('\d+ \d+ (.*) -\d',line)[0]
+		sp_start,sp_end = re.findall('(^\d+) \d',line)[0],re.findall('^\d+ (\d+) \D',line)[0]
+		file_list.append((int(sp_start),int(sp_end),word))
+in_f.close()
+#print input_dict['\"\'*\'/TestingSample410.rec\"\n']
+		
+# Do the cross matching
+
+for file in list_file_nums:
+	input_predictions = input_dict[str(file)]
+	sil_list = sil_dict[str(file)]
+	input_list = input_predictions[1:-1]
+	complete_list = []
+	for s in range(0,len(sil_list)-1):
+		sil_start,sil_end,sil_word = sil_list[s][0],sil_list[s][1],sil_list[s][2]
+		next_sil_start,next_sil_end,next_sil_word = sil_list[s+1][0],sil_list[s+1][1],sil_list[s+1][2]
+		complete_list.append((sil_start,sil_end,sil_word))
+		for i in input_list:
+			speech_start,speech_end,speech_word = i[0],i[1],i[2]
+			if (speech_start > (sil_end)) & (speech_start < (next_sil_start)):
+				complete_list.append((i[0],i[1],i[2]))
+
+	complete_list.append((sil_list[-1][0],sil_list[-1][1],sil_list[-1][2]))
+	
+	out_f.write(input_predictions[0])
+	for i in range(0,len(complete_list)):
+		start,end,word = complete_list[i][0],complete_list[i][1],complete_list[i][2]
+		out_f.write("%s %s %s\n" %(start,end,word))
+	out_f.write(input_predictions[-1])
+out_f.close()
+
+#
